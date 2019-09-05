9c1e61fa740862708c5c75ab6bfb49b5c7e8d73c
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-09-05 14:19:59 +0900

    Fix @actions/exec

--- src/installer.ts
+++ src/installer.ts
@@ -4,6 +4,6 @@ import * as semver from 'semver';
 
 export async function installPhp(version: string) {
   if (process.platform === 'linux') {
-    await exec(path.join(__dirname, 'install-php-ubuntu.sh'), [version]);
+    await exec.exec(path.join(__dirname, 'install-php-ubuntu.sh'), [version]);
   }
 }
