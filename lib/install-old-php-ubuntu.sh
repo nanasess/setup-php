55fd3036dbcb3951a2d3eff283d4bdd76cece8e1
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-09-15 18:03:29 +0900

    Modify workflow

--- lib/install-old-php-ubuntu.sh
+++ lib/install-old-php-ubuntu.sh
@@ -8,22 +8,12 @@ version=$1
 sudo apt-get update
 sudo apt-get purge 'php*'
 
-if [[ ! -d ~/.phpenv ]]
-then
-    git clone https://github.com/phpenv/phpenv.git ~/.phpenv
-    echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.bash_profile
-    echo 'eval "$(phpenv init -)"' >> ~/.bash_profile
-    exec $SHELL -l
-fi
+export PATH="$HOME/.phpenv/bin:$PATH"
+eval "$(phpenv init -)"
 
-PHP_BUILD_ROOT=$(phpenv root)/plugins/php-build
-if [[ ! -d $PHP_BUILD_ROOT ]]
-then
-    git clone https://github.com/php-build/php-build $PHP_BUILD_ROOT
-    PHP_BUILD_EXTRA_MAKE_ARGUMENTS="-j$(nproc)"
-    PHP_BUILD_CONFIGURE_OPTS="--with-freetype-dir=/usr --with-pdo-pgsql"
-    PHP_BUILD_KEEP_OBJECT_FILES="on"
-fi
+export PHP_BUILD_EXTRA_MAKE_ARGUMENTS="-j$(nproc)"
+export PHP_BUILD_CONFIGURE_OPTS="--with-freetype-dir=/usr --with-pdo-pgsql"
+export PHP_BUILD_KEEP_OBJECT_FILES="on"
 
 case "$version" in
     "5.4" )
