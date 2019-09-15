ff4151bc85a82a991b6b971094f4969a116966a8
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-09-15 17:33:26 +0900

    Add options

--- lib/install-old-php-ubuntu.sh
+++ lib/install-old-php-ubuntu.sh
@@ -22,14 +22,15 @@ then
     git clone https://github.com/php-build/php-build $PHP_BUILD_ROOT
     PHP_BUILD_EXTRA_MAKE_ARGUMENTS="-j$(nproc)"
     PHP_BUILD_CONFIGURE_OPTS="--with-freetype-dir=/usr --with-pdo-pgsql"
+    PHP_BUILD_KEEP_OBJECT_FILES="on"
 fi
 
 case "$version" in
     "5.4" )
-        phpenv install 5.4.45
+        phpenv install -v -s 5.4.45
         ;;
     "5.5" )
-        phpenv install 5.5.38
+        phpenv install -v -s 5.5.38
         ;;
 esac
 
