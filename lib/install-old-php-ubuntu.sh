523b36571773914d1f04a07d2c92a256a74fa7c1
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-09-17 01:53:41 +0900

    Remove

--- lib/install-old-php-ubuntu.sh
+++ lib/install-old-php-ubuntu.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 # sudo apt-get purge 'php*'
 sudo apt-get install -y libcurl4-nss-dev libjpeg-dev re2c libxml2-dev \
      libtidy-dev libxslt-dev libmcrypt-dev libreadline-dev libfreetype6-dev \
-     zlib1g-dev libzip-dev mysql-client
+     libpq-dev libpq5 zlib1g-dev libzip-dev mysql-client
 
 sudo ln -s /usr/include/x86_64-linux-gnu/curl /usr/local/include/curl
 
