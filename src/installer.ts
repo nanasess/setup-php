d71465439c20baddd19344dad2944a5c8b619a73
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-09-25 12:26:39 +0900

    Add installer on windows

--- src/installer.ts
+++ src/installer.ts
@@ -11,5 +11,7 @@ export async function installPhp(version: string) {
         version
       ]);
     }
+  } else if (process.platform === 'win32') {
+    await exec.exec(path.join(__dirname, 'install-php-windows.ps1'));
   }
 }
