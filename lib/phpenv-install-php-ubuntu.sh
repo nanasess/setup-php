ed05d5fcb479dcec7a6fdc84060867842a6631ca
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-11-27 15:04:16 +0900

    Add libonig-dev

--- lib/phpenv-install-php-ubuntu.sh
+++ lib/phpenv-install-php-ubuntu.sh
@@ -53,7 +53,7 @@ then
 fi
 sudo apt-fast install -y libcurl4-nss-dev libjpeg-dev re2c libxml2-dev \
      libtidy-dev libxslt-dev libmcrypt-dev libreadline-dev libfreetype6-dev \
-     zlib1g-dev libzip-dev mysql-client
+     libonig-dev zlib1g-dev libzip-dev mysql-client
 
 sudo ln -s /usr/include/x86_64-linux-gnu/curl /usr/local/include/curl
 
